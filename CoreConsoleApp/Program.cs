52f470a2a386dc61851cbb36d0f84b832a700291
David Ebbo <david.ebbo@gmail.com>
2017-02-13 18:18:16 -0800

    Add AppSettings and Connection Strings

--- CoreConsoleApp/Program.cs
+++ CoreConsoleApp/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace CoreConsoleApp
 {
     class Program
     {
+        static public IConfigurationRoot Configuration { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            Console.WriteLine($"foo = {Configuration["foo"]}");
+            Console.WriteLine($"MyDatabase = {Configuration.GetConnectionString("MyDatabase")}");
         }
     }
 }
